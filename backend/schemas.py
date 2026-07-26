611d5468174e8d99ae40b5441f632e1df894c58e
Alejandro <alexosorno991@gmail.com>
2026-07-26 17:41:56 -0600

    Revert "Bloqueo"
    This reverts commit 527aab721992f2e4bf6915a3581b4574706cab35.

--- backend/schemas.py
+++ backend/schemas.py
@@ -90,8 +90,7 @@ class OrdenItemCreate(BaseModel):
     modificador_id: Optional[int] = None
     cantidad: int = 1
     comentario: Optional[str] = None
-    comensal: Optional[str] = "C1"  # 👈 Cambiado a Optional[str] con valor por defecto "C1"
-    precio_unitario: Optional[float] = None  
+    comensal: int = 1  # Por defecto si no se manda, será el comensal 1
 
 
 class OrdenItemOut(BaseModel):
@@ -101,11 +100,10 @@ class OrdenItemOut(BaseModel):
     modificador_id: Optional[int]
     modificador_nombre: Optional[str]
     cantidad: int
-    precio_unitario: Optional[float] = None
+    precio_unitario: float
     comentario: Optional[str]
     estado_cocina: str
     estacion: str
-    comensal: Optional[str] = "C1"  # 👈 Cambiado a Optional[str]
 
     class Config:
         from_attributes = True
